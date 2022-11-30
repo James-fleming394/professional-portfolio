d96dbe0b6bebddfde22cdb3a3f9dc83ab106d21f
james-fleming394 <james.fleming394@gmail.com>
2022-11-30 13:05:34 -0500

    Updated navbar functionality in place

--- professional-portfolio/src/App.js
+++ professional-portfolio/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import NavBar from './components/Nav';
+import NavBar from './components/NavBar';
 
 
 function App() {
---
+++ professional-portfolio/src/components/NavBar.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { useState, useEffect } from "react";
+
+const NavBar = () => {
+    const [activeUser, setActiveUser] = useState('/');
+    const [userScroll, setUserScroll] = useState(false);
+
+    useEffect(() => {
+        const onScroll = () => {
+            if (window.scrollY > 50) {
+                setUserScroll(true);
+            } else {
+                setUserScroll(false);
+            }
+        }
+        window.addEventListener("scroll", onScroll);
+
+        return () => window.removeEventListener("scroll", onScroll);
+    }, [])
+
+    const onUpdateActiveUser = (value) => {
+        setActiveUser(value);
+    }
+
+
+    return (
+    <Navbar expand="lg" className={userScroll ? "userScroll": ""}>
+        <Container>
+        <Navbar.Brand href="/">Logo goes here</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggle-icon">Icon goes here</span>
+            </Navbar.Toggle> 
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+            <Nav.Link href="/" className={activeUser === '/' ? 'active-navbar-link': 'navbar-link'} onClick={() => onUpdateActiveUser('/')}>Home</Nav.Link>
+            <Nav.Link href="/about" className={activeUser === 'about' ? 'active-navbar-link': 'navbar-link'} onClick={() => onUpdateActiveUser('about')}>About</Nav.Link>
+            <Nav.Link href="/projects" className={activeUser === 'projects' ? 'active-navbar-link': 'navbar-link'} onClick={() => onUpdateActiveUser('projects')}>Projects</Nav.Link>
+            </Nav>
+        <span className="navbar-text">
+            <div className="social-icons">
+                <a href="https://www.linkedin.com/in/james--fleming/">LinkedIn Icon Here</a>
+                <a href="https://github.com/James-fleming394">Github Icon Here</a>
+            </div>
+            <button className="connect-button" onClick={() => console.log('working')}><span>Connect</span></button>
+        </span>
+        </Navbar.Collapse>
+        </Container>
+    </Navbar>
+    )
+}
+
+export default NavBar
+
