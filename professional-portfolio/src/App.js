6e18d43208e4801492d8a670e6b3314a4713369f
james-fleming394 <james.fleming394@gmail.com>
2022-11-30 12:39:23 -0500

    Change to Navbar made

--- professional-portfolio/src/App.js
+++ professional-portfolio/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import NavBar from './components/Nav';
 
+
 function App() {
 
 
--- professional-portfolio/src/components/Nav.jsx
+++ professional-portfolio/src/components/Nav.jsx
@@ -1,59 +1,33 @@
-import React from 'react';
+import React from "react";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { useState, useEffect } from 'react';
-
 
 const NavBar = () => {
-
-    const [activeUser, setActiveUser] = useState('home');
-    const [userScroll, setUserScroll] = useState(false);
-
-    useEffect(() => {
-        const onScroll = () => {
-            if (window.scrollY > 50) {
-                setUserScroll(true)
-            } else {
-                setUserScroll(false)
-            }
-        }
-
-        window.addEventListener("scroll", onScroll);
-
-        return() => window.removeEventListener("scroll", onScroll);
-    }, []);
-
-    const onUpdateActiveUser = (value) => {
-        setActiveUser(value);
-    }
-
     return (
-    <Navbar bg="light" expand="lg" className={userScroll ? "scroll" : ""}>
-    <Container>
-        <Navbar.Brand href="/">
-            <img src="https://i.imgur.com/PbKVQzs.jpg" alt="Logo" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <span className="navbar-toggler-icon"></span>
+    <Navbar expand="lg">
+        <Container>
+        <Navbar.Brand href="/">Logo goes here</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggle-icon">Icon goes here</span>
+            </Navbar.Toggle> 
         <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-            <Nav.Link href="/" className={activeUser === '/' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveUser('/')}>Home</Nav.Link>
-            <Nav.Link href="/about" className={activeUser === '/' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveUser('/about')}>About</Nav.Link>
-            <NavDropdown title="Projects ▼" id="basic-nav-dropdown" className={activeUser === '/' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveUser('/projects')}>
-            <NavDropdown.Item href="/project1">Project 1 Title</NavDropdown.Item>
-            <NavDropdown.Item href="/project2">Project 2 Title</NavDropdown.Item>
-            <NavDropdown.Item href="/project3">Project 3 Title</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="/futureprojects">Future Projects</NavDropdown.Item>
-            </NavDropdown>
-        </Nav>
+            <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/projects">Projects</Nav.Link>
+            </Nav>
+        <span className="navbar-text">
+            <div className="social-icons">
+                <a href="https://www.linkedin.com/in/james--fleming/">LinkedIn Icon Here</a>
+                <a href="https://github.com/James-fleming394">Github Icon Here</a>
+            </div>
+        </span>
         </Navbar.Collapse>
-    </Container>
+        </Container>
     </Navbar>
     )
 }
 
-export default NavBar;
+export default NavBar
 
