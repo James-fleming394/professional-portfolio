982bd8d7558b257cf3cfdefd07a2b31aa1ce1e78
james-fleming394 <james.fleming394@gmail.com>
2022-12-01 12:52:38 -0500

    Projects file create

--- professional-portfolio/src/App.js
+++ professional-portfolio/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Banner from './components/Banner';
 import Skills from './components/Skills';
+import Projects from './components/Projects';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -15,6 +16,7 @@ function App() {
     <div>
       <Banner />
       <Skills />
+      <Projects />
       </div></>
   );
 }
