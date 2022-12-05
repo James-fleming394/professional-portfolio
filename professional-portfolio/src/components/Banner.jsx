937bb197435040bf94fbf7c2e542ea23b610b05a
james-fleming394 <james.fleming394@gmail.com>
2022-12-05 11:56:51 -0500

    Clean up of code

--- professional-portfolio/src/components/Banner.jsx
+++ professional-portfolio/src/components/Banner.jsx
@@ -1,49 +1,9 @@
 import React from "react";
-// import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap"
 import headerImg from '../assets/img/header.png'
+import 'animate.css';
 
 const Banner = () => {
-    // const [loopNum, setLoopNum] = useState(0);
-    // const [isDelete, setIsDelete] = useState(false);
-    // const toRotate = [ "Full-Stack Developer", "Web Developer", "Software Engineer" ];
-    // const [text, setText] = useState('');
-    // const [index, setIndex] = useState(1);
-    // const [delta, setDelta] = useState(300 - Math.random() * 100)
-    // const period = 2000;
-
-    // useEffect(() => {
-    //     let timer = setInterval(() => {
-    //         time();
-    //     }, delta)
-        
-    //     return () => {clearInterval(timer)};
-    // }, [text])
-
-    // const time = () => {
-    //     let i = loopNum % toRotate.length;
-    //     let fullText = toRotate[i];
-    //     let updatedText = isDelete ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-
-    //     setText(updatedText);
-
-    //     if (isDelete) {
-    //         setDelta(prevDelta => prevDelta /2)
-    //     }
-
-    //     if (!isDelete ?? updatedText === fullText) {
-    //         setIsDelete(true);
-    //         setIndex(prevIndex => prevIndex -1)
-    //         setDelta(period);
-    //     } else if (isDelete ?? updatedText === '') {
-    //         setIsDelete(false);
-    //         setLoopNum(loopNum + 1);
-    //         setIndex(1)
-    //         setDelta(500);
-    //     } else {
-    //         setIndex(prevIndex => prevIndex + 1)
-    //     }
-    // }
 
     return (
         <section className="homepage" id="home">
