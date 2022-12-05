68e8f24e7d32eb914b5099991df9e079e2c524ec
james-fleming394 <james.fleming394@gmail.com>
2022-12-05 11:22:18 -0500

    Footer logic complete

--- professional-portfolio/src/App.js
+++ professional-portfolio/src/App.js
@@ -4,6 +4,7 @@ import Banner from './components/Banner';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -19,6 +20,9 @@ function App() {
       <Skills />
       <Projects />
       <Contact />
+      </div>
+      <div>
+        <Footer />
       </div></>
   );
 }
--- professional-portfolio/src/components/Footer.jsx
+++ professional-portfolio/src/components/Footer.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import laptop from '../assets/img/laptop-code.png';
+import icon from '../assets//img/github-logo-icon.png'
+import icon1 from '../assets/img/linkedin-logo-icon.png'
 
 const Footer = () => {
-
+    return (
+        <footer className="footer">
+            <Container>
+                <Row className="align-item-center">
+                    <Col sm={6}>
+                        <img src={laptop} alt="Logo" />
+                    </Col>
+                    <Col sm={6} className="text-center-sm-end">
+                        <div className="social-icons">
+                            <a href="https://www.linkedin.com/in/james--fleming/"><img src={icon1} alt="LinkedIn"></img></a>
+                            <a href="https://github.com/James-fleming394"><img src={icon} alt="Github"></img></a>
+                        </div>
+                        <p>A Website by James Fleming, Full-Stack Developer</p>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    )
 }
 
 
